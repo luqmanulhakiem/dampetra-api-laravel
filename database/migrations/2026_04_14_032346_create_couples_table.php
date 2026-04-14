ae20bb58f49d2af0ab49da9b28e85d33930d0700
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 10:25:05 +0700

    feat: add couple migration, model and seeder

---
+++ database/migrations/2026_04_14_032346_create_couples_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('couples', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('man_id')->constrained('users')->onDelete('cascade')->nullable();
+            $table->foreignId('woman_id')->constrained('users')->onDelete('cascade')->nullable();
+            $table->date("anniv_date")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('couples');
+    }
+};
