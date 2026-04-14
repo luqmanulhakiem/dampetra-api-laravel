836e8de9f260ad84577b03b813552db868042f0f
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 10:15:41 +0700

    feat: add daily migrations and seeeder

---
+++ database/migrations/2026_04_14_031341_create_daily_emotions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_emotions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('mood', ['Angry', 'Sad', 'Neutral', 'Happy', 'Excited']);
+            $table->text("notes")->nullable();
+            $table->date('log_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_emotions');
+    }
+};
