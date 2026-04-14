12c19c3bdbce8e864ebb0582057ab617af948131
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 14:20:49 +0700

    feat: add login request

--- app/Http/Requests/API/LoginRequest.php
+++ app/Http/Requests/API/LoginRequest.php
@@ -12,7 +12,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,8 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "email" => "required|string|email|max:255",
+            "password" => "required|string|min:8|alpha_num",
         ];
     }
 }
