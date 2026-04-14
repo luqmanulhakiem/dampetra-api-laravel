cac989cee2629a0efb8be82779476b01f696a080
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 10:18:39 +0700

    feat: add daily emotion relation to user model

--- app/Models/DailyEmotion.php
+++ app/Models/DailyEmotion.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DailyEmotion extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
