560eabd214d448171e34ac2988b840979e2a4e73
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 11:07:30 +0700

    feat: add auth controller

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //TODO: Implement registration logic
+    }
+
+    public function login(Request $request)
+    {
+        //TODO: Implement login logic
+    }
+
+    public function logout(Request $request)
+    {
+        // TODO: Implement logout logic
+    }
+}
