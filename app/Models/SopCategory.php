1b6ad4b8b6994e5945d22154de3231db60751a5a
Luqmanul Hakiem <luqmanul@ebconnect.com>
2026-04-14 10:39:22 +0700

    feat: add sop couple relation to sop category

--- app/Models/SopCategory.php
+++ app/Models/SopCategory.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SopCategory extends Model
 {
     protected $guarded = ['id'];
+
+    function sopCouple(): BelongsTo
+    {
+        return $this->belongsTo(SopCouple::class, 'id', 'category_id');
+    }
 }
--- app/Models/SopCouple.php
+++ app/Models/SopCouple.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class SopCouple extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * Get Sop Category
+     * @return HasOne
+     */
+    function category(): HasOne
+    {
+        return $this->hasOne(SopCategory::class, 'id', 'category_id');
+    }
 }
